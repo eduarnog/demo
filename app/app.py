6f6f2a71ef496d2a35388f56a52f3d9115af4171
eduarnog <126154429+eduarnog@users.noreply.github.com>
2023-04-12 21:13:45 +0000

    v.001.003

--- app/app.py
+++ app/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from . import cookies, simple_pages
+from app.extensions.database import db, migrate
 
-app = Flask(__name__)
-app.config.from_object('app.config')
+def create_app():
+  app = Flask(__name__)
+  app.config.from_object('app.config')
+
+  register_extensions(app)
+  register_blueprints(app)
+
+  return app
 
 # Blueprints
-app.register_blueprint(cookies.routes.blueprint)
-app.register_blueprint(simple_pages.routes.blueprint)
+def register_blueprints(app: Flask):
+  app.register_blueprint(cookies.routes.blueprint)
+  app.register_blueprint(simple_pages.routes.blueprint)
+
+# Extensions
+def register_extensions(app: Flask):
+  db.init_app(app)
+  migrate.init_app(app, db, compare_type=True)
