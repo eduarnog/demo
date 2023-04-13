f5fb1d66cd72c284ab15a12bc4d612639a03c112
eduarnog <126154429+eduarnog@users.noreply.github.com>
2023-04-13 12:46:00 +0000

    v.001.007

--- app/users/routes.py
+++ app/users/routes.py
@@ -61,7 +61,7 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('simple_pages.dashboard'))
+            return render_template('simple_pages/dashboard.html')
         user.save()
 
     return render_template("users/register.html", user=current_user)
